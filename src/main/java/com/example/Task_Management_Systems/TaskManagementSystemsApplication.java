2f08d0c31e3de5c77b2c60f6bfd646b846c142bd
SeeXWH <plarroar03050@gmail.com>
2024-08-16 13:21:13 +0300

    qwe

---
+++ src/main/java/com/example/Task_Management_Systems/TaskManagementSystemsApplication.java
@@ -0,0 +1,14 @@
+package com.example.Task_Management_Systems;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition
+@SpringBootApplication
+public class TaskManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Task_Management_Systems/TaskManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management_Systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
